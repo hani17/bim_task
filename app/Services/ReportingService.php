4e1e1d68de124af3bf80ffaf4dc01071a68be264
hany.farag <hani.farag@appetito.com>
2023-11-23 20:51:15 +0200

    fix generating report
    - generating report based on actual payment and paid_on

--- app/Services/ReportingService.php
+++ app/Services/ReportingService.php
@@ -10,17 +10,35 @@ class ReportingService
 {
     public function generateReport(string $startDate, string $endDate)
     {
-        $reports = Transaction::select(
-            DB::raw('MONTH(due_on) as month'),
-            DB::raw('YEAR(due_on) as year'),
-            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::PAID->value .' THEN total_paid_amount ELSE 0 END) as paid'),
-            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::OUTSTANDING->value .' THEN (amount + vat_amount) - total_paid_amount ELSE 0 END) as outstanding'),
-            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::OVERDUE->value .' THEN (amount + vat_amount) - total_paid_amount ELSE 0 END) as overdue')
-        )
-            ->whereBetween('due_on', [$startDate, $endDate])
-            ->groupBy(DB::raw('YEAR(due_on)'), DB::raw('MONTH(due_on)'))
+        $reports = Transaction::join('payments', 'transactions.id', '=', 'payments.transaction_id')
+            ->select(
+                DB::raw('MONTH(payments.paid_on) as month'),
+                DB::raw('YEAR(payments.paid_on) as year'),
+                DB::raw('SUM(payments.amount) as paid'),
+                DB::raw('SUM(CASE WHEN transactions.due_on > NOW() AND (transactions.amount + transactions.vat_amount) > transactions.total_paid_amount  THEN (transactions.amount + transactions.vat_amount) - transactions.total_paid_amount ELSE 0 END) as outstanding'),
+                DB::raw('SUM(CASE WHEN transactions.due_on < NOW() AND (transactions.amount + transactions.vat_amount) > transactions.total_paid_amount  THEN (transactions.amount + transactions.vat_amount) - transactions.total_paid_amount ELSE 0 END) as overdue')
+            )
+            ->whereBetween('payments.paid_on', [$startDate, $endDate])
+            ->groupBy(DB::raw('YEAR(payments.paid_on)'), DB::raw('MONTH(payments.paid_on)'))
+            ->orderBy(DB::raw('YEAR(payments.paid_on)'))
             ->get();
 
         return $reports->toArray();
     }
+
+//    public function generateReport(string $startDate, string $endDate)
+//    {
+//        $reports = Transaction::select(
+//            DB::raw('MONTH(due_on) as month'),
+//            DB::raw('YEAR(due_on) as year'),
+//            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::PAID->value .' THEN total_paid_amount ELSE 0 END) as paid'),
+//            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::OUTSTANDING->value .' THEN (amount + vat_amount) - total_paid_amount ELSE 0 END) as outstanding'),
+//            DB::raw('SUM(CASE WHEN status = '. TransactionStatus::OVERDUE->value .' THEN (amount + vat_amount) - total_paid_amount ELSE 0 END) as overdue')
+//        )
+//            ->whereBetween('due_on', [$startDate, $endDate])
+//            ->groupBy(DB::raw('YEAR(due_on)'), DB::raw('MONTH(due_on)'))
+//            ->get();
+//
+//        return $reports->toArray();
+//    }
 }
